979fc60c4867dcb3d08cee9ee38cb3cfa3a2bcf2
Matthew Mueller <mattmuelle@gmail.com>
2014-12-30 02:48:10 -0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,54 @@
+
+# superjson
+
+  Extends JSON.stringify and JSON.parse to support additional JS types (Dates, RegExps, Functions, etc)
+
+## Goal
+
+  The goal of this project is to properly serialize and deserialize JSON structures. It'd be nice if this
+  was as fast as possible, but consistent data types in and out is more important. If it's impossible
+  to consistently serialize or deserialize an object, the library should throw.
+
+## Example
+
+```js
+var json = require('superjson');
+
+var str = json.stringify({
+  date: new Date(),
+  regexp: /[abc]d/,
+  fn: function foo() {}
+})
+
+json.parse(str)
+{
+  date: date, // date instance
+  regexp: /[abc]d/, // regexp instance
+  fn: [Function foo] // foo function
+}
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2014 Matthew Mueller &lt;matt@lapwinglabs.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,116 @@
+var type = require('component-type');
+var isArray = Array.isArray;
+var json = require('json3');
+
+/**
+ * Regexps
+ */
+
+// https://github.com/moment/moment/blob/583f380740468db843154df31fa72647c6173d17/moment.js#L72
+var rdate = /^\s*(?:[+-]\d{6}|\d{4})-(?:(\d\d-\d\d)|(W\d\d$)|(W\d\d-\d)|(\d\d\d))((T| )(\d\d(:\d\d(:\d\d(\.\d+)?)?)?)?([\+\-]\d\d(?::?\d\d)?|\s*Z)?)?$/;
+var rregexp = /^\/|\/([gimy]*)$/g;
+
+/**
+ * Stringify
+ *
+ * TODO: support other `replacer` types
+ *
+ * @param {Object} obj
+ * @param {Function} replacer
+ * @param {Number} spaces
+ * @return {String}
+ * @api public
+ */
+
+var stringify = exports.stringify = function (obj, replacer, space) {
+  return json.stringify(obj, function(k, v) {
+    return replacer ? replacer(k, coerce(k, v)) : coerce(k, v);
+  }, space);
+}
+
+/**
+ * Parse
+ *
+ * TODO: support other `reviver` types
+ *
+ * @param {String} str
+ * @param {Function} reviver
+ * @return {Object}
+ * @api public
+ */
+
+var parse = exports.parse = function(str, reviver) {
+  return json.parse(str, function(k, v) {
+    return reviver ? reviver(k, revive(k, v)) : revive(k, v);
+  });
+}
+
+/**
+ * Coerce object to a string
+ *
+ * @param {String} k
+ * @param {Mixed} v
+ * @return {String}
+ */
+
+function coerce(k, v) {
+  switch(type(v)) {
+    case 'date': return v.toISOString();
+    case 'regexp': return v.toString();
+    case 'function': return v.toString();
+    default: return v;
+  }
+}
+
+/**
+ * Revive string to an object
+ *
+ * @param {String} k
+ * @param {Mixed} v
+ * @return {Mixed}
+ */
+
+function revive(k, v) {
+  if ('string' != type(v)) return v;
+  if (rdate.test(v)) return stod(v);
+  if ('/' == v[0] && rregexp.test(v)) return stor(v);
+  if ('function' == v.slice(0, 8) && '}' == v[v.length - 1]) return stof(v);
+  return v;
+}
+
+/**
+ * String => Date
+ *
+ * @param {String} str
+ * @return {Date}
+ */
+
+function stod(str) {
+  var d = Date.parse(str);
+  if (isNaN(d)) throw new Error('cannot convert string to date');
+  return new Date(str);
+}
+
+/**
+ * String => Regexp
+ *
+ * @param {String} str
+ * @return {RegExp}
+ */
+
+function stor(str) {
+  var i = str.lastIndexOf('/');
+  var flags = str.slice(i + 1);
+  return new RegExp(str.slice(1, i), flags);
+}
+
+/**
+ * String => Function
+ *
+ * @param {String} str
+ * @return {Function}
+ */
+
+function stof(str) {
+  return new Function('return ' + str)();
+}
