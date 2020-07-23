c617bc76a538dcdd182c0b31b08e2b3a7bce4ea0
Dylan Brookes <24858006+merelinguist@users.noreply.github.com>
2020-07-23 20:50:28 +0100

    Implement custom type checking (#13)
    Co-authored-by: merelinguist <merelinguist@users.noreply.github.com>

--- src/pathstringifier.ts
+++ src/pathstringifier.ts
@@ -5,10 +5,7 @@ const escape = (key: string) => key.replace(/\./g, '\\.');
 const unescape = (k: string) => k.replace(/\\\./g, '.');
 
 export const stringifyPath = (path: Path): StringifiedPath =>
-  path
-    .map(String)
-    .map(escape)
-    .join('.');
+  path.map(String).map(escape).join('.');
 
 export const parsePath = (string: StringifiedPath): Path =>
   string.split(/(?<!\\)\./g).map(unescape);
