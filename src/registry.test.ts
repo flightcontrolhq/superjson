c85803a9ea24e18e4b5a81bae648f54919b12583
Simon Knott <info@simonknott.de>
2022-12-06 13:57:05 +0100

    remove warn test

--- src/registry.test.ts
+++ src/registry.test.ts
@@ -16,14 +16,6 @@ test('class registry', () => {
 
   expect(() => registry.register(Car)).not.toThrow();
 
-  const warnSpy = jest.spyOn(console, 'debug');
-
-  registry.register(class Car {});
-  expect(warnSpy).toHaveBeenCalledTimes(1);
-  expect(warnSpy.mock.calls[0][0]).toContain(
-    'Ambiguous class "Car", provide a unique identifier.'
-  );
-
   registry.register(class Car {}, 'car2');
 
   expect(registry.getValue('car2')).not.toBeUndefined();
